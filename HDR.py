d49846547dfc2223f6f3e21b0b11ba6c3010e95b
Jacob Hann <contentdeleted@gmail.com>
2019-03-11 02:21:04 +0000

    wip: started copying hdr stuff

--- HDR.py
+++ HDR.py
@@ -1,2 +1,40 @@
-
 import gphoto2 as gp
+import cv2 
+
+# In the future these exposures will come from camera ( same with image data )
+def readImagesAndTimes():
+  # List of exposure times
+  times = np.array([ 1/30.0, 0.25, 2.5, 15.0 ], dtype=np.float32)
+   
+  # List of image filenames
+  filenames = ["img_0.033.jpg", "img_0.25.jpg", "img_2.5.jpg", "img_15.jpg"]
+  images = []
+  for filename in filenames:
+    im = cv2.imread(filename)
+    images.append(im)
+   
+  return images, times
+
+
+def main():
+    images, times = readImagesAndTimes()
+    
+    # Align input images
+    alignMTB = cv2.createAlignMTB()
+    alignMTB.process(images, images)
+    
+    # Obtain Camera Response Function (CRF)
+    calibrateDebevec = cv2.createCalibrateDebevec()
+    responseDebevec = calibrateDebevec.process(images, times)
+
+    # Merge images into an HDR linear image
+    mergeDebevec = cv2.createMergeDebevec()
+    hdrDebevec = mergeDebevec.process(images, times, responseDebevec)
+    # Save HDR image.
+    cv2.imwrite("hdrDebevec.hdr", hdrDebevec)
+
+    # Tonemap using Drago's method to obtain 24-bit color image
+    tonemapDrago = cv2.createTonemapDrago(1.0, 0.7)
+    ldrDrago = tonemapDrago.process(hdrDebevec)
+    ldrDrago = 3 * ldrDrago
+    cv2.imwrite("ldr-Drago.jpg", ldrDrago * 255)
